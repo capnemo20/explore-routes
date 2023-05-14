24953c73ecf1f05a53300b293019b7d8512b4e2c
capnemo20 <z.sujon20@outlook.com>
2023-05-14 01:29:59 +0100

    52-1 Module Introduction, simple react router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const router = createBrowserRouter([
+    {path:"/", element:<div>This is default page</div>},
+    {path:"/home", element:<div>Home Page</div>},
+    {path:"/about", element:<div>This is About Page</div>}
+  ])
   return (
     <div className="App">
+      <RouterProvider
+      router={router}
       
+
+      ></RouterProvider>
     </div>
   );
 }
